86129486aa21a574517d8a3a957380174e02debc
Thane Vo <vothane@gmail.com>
2015-05-19 00:39:02 +0000

    add neural network code

---
+++ test/bullrocket/Models/nn_test.clj
@@ -0,0 +1,26 @@
+(ns bullrocket.Models.nn-test
+  (:require [clojure.test :refer :all]
+            [bullrocket.Models.nn :refer :all]))
+
+(def data [[[0 0] [0]]
+           [[0 1] [1]]
+           [[1 0] [1]]
+           [[1 1] [0]]])
+
+
+(def default-options
+  {:max-iters 100
+   :desired-error 0.20
+   :hidden-neurons [3]
+   :learning-rate 0.3
+   :learning-momentum 0.01
+   :weight-epsilon 50})
+
+(def NN (train data))
+
+(deftest run-binary-test
+  (testing "finding closet data point"
+    (is (=  (run-binary NN  [0 1]) [1]))
+    (is (=  (run-binary NN  [1 0]) [1]))
+    (is (=  (run-binary NN  [0 0]) [0]))
+    (is (=  (run-binary NN  [1 1]) [1]))))
