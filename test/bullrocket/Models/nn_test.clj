2cdced6264440f8ab874e5e15064412bfee22118
vothane <vothane@gmail.com>
2015-08-19 01:49:26 +0000

    add in the enclog lib and check to see if everything works

--- test/bullrocket/Models/nn_test.clj
+++ test/bullrocket/Models/nn_test.clj
@@ -1,29 +1,31 @@
 (ns bullrocket.Models.nn-test
   (:require [clojure.test :refer :all]
-            [bullrocket.Models.nn :refer :all]))
+            [bullrocket.Models.nn :refer :all])
+  (:use     [enclog nnets training]))
 
-(def data [[[0 0] [0]]
-           [[0 1] [1]]
-           [[1 0] [1]]
-           [[1 1] [0]]])
+(def mlp (network (neural-pattern :feed-forward) 
+                   :activation :sigmoid
+                   :input 2
+                   :output 1
+                   :hidden [3]))
 
-(def default-options
-  {:max-iters 100
-   :desired-error 0.20
-   :hidden-neurons [3]
-   :learning-rate 0.3
-   :learning-momentum 0.01
-   :weight-epsilon 50})
+(defn train-nn [nn data alg]
+  (let [trainer (trainer alg :network nn :training-set data)]
+    (train trainer 0.01 250 [])))
 
-;(defn train [samples]
-;  (let [network (MultiLayerPerceptron. default-options)]
-;    (train-ann network samples)))
+(def dataset
+  (let [x [[0.0 0.0] [1.0 0.0] [0.0 1.0] [1.0 1.0]]
+        y [[0.0] [1.0] [1.0] [0.0]]]
+    (data :basic-dataset x y)))
 
-;(def NN (train data))
 
-;(deftest run-binary-test
-;  (testing "finding closet data point"
-;    (is (=  (run-binary NN  [0 1]) [1]))
-;    (is (=  (run-binary NN  [1 0]) [1]))
-;    (is (=  (run-binary NN  [0 0]) [0]))
-;    (is (=  (run-binary NN  [1 1]) [1]))))
+(def MLP (train-nn mlp dataset :back-prop))
+
+(defn run-nn [nn input]
+  (let [in  (data :basic input)
+        out (.compute nn in)]
+    (.getData out)))
+
+(deftest nn-test
+  (testing "experiment"
+    (is (= (run-nn MLP [0 0]) [0]))))
