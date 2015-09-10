26dd69b0f1b0824467d4fc507bb1237b0face0eb
vothane <vothane@gmail.com>
2015-09-10 00:18:51 +0000

    call the right fn

--- test/bullrocket/Models/nn_test.clj
+++ test/bullrocket/Models/nn_test.clj
@@ -1,11 +1,10 @@
 (ns bullrocket.Models.nn-test
   (:require [clojure.test :refer :all]
-            [bullrocket.Models.nn :refer :all])
-  (:use     [enclog nnets training]))
+            [bullrocket.Models.nn :refer :all]))
 
 (deftest parse-csv-test
   (testing "should parse csv"
     (let [f ["test/bullrocket/data/FXA.csv" "test/bullrocket/data/FXB.csv" "test/bullrocket/data/FXC.csv"]
           t "test/bullrocket/data/GLD.csv" 
-          d (data f t)]
+          d (normalize f t)]
       (is (= d [])))))
