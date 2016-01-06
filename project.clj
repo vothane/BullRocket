6b8ea517cd973dbdf92bc3fa165729378f6ec7fb
Thane Vo <vothane@gmail.com>
2016-01-06 02:03:34 +0000

    add incanter zoo for time series calculations of stocks

--- project.clj
+++ project.clj
@@ -6,4 +6,6 @@
   :dependencies [[org.clojure/clojure "1.6.0"]
                  [org.encog/encog-core "3.2.0"]
                  [enclog "0.6.5"]
-                 [org.clojure/data.csv "0.1.3"]])
+                 [org.clojure/data.csv "0.1.3"]
+                 [incanter "1.9.0"]
+                 [incanter/incanter-zoo "1.9.0"]])
--- src/bullrocket/Models/kmeans.clj
+++ src/bullrocket/Models/kmeans.clj
@@ -1,1 +1,24 @@
-(ns bullrocket.Models.kmeans)
+(ns bullrocket.Models.kmeans
+  (:require [incanter.core :as i]
+            [incanter.io]
+            [incanter.zoo :as zoo]
+            [clj-time.format :as tf]))
+
+(def ^:dynamic *formatter* (tf/formatter "dd-MMM-yy"))
+(defn parse-date [date] (tf/parse *formatter* date))
+
+(def data
+  (i/add-derived-column
+    :date [:date-str] parse-date
+    (i/col-names
+      (incanter.io/read-dataset data-file)
+      [:date-str :open :high :low :close :volume])))
+
+(def data-zoo (zoo/zoo data :date))
+
+(def data-roll5
+  (->>
+    (i/sel data-zoo :cols :close)
+    (zoo/roll-mean 5)
+    (i/dataset [:five-day])
+    (i/conj-cols data-zoo)))
